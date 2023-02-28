27c19da2a4daf44394c26200e5ba25ea37be3ca7
iambinod <helloitsbinod@gmail.com>
2023-02-28 15:43:16 +0545

    feat: added a new component filterTab

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -1,5 +1,6 @@
 import Breadcrumb from "../components/Breadcrumb";
 import Card from "../components/Card";
+import FilterTab from "../components/FilterTab";
 
 const Filter = () => {
   return (
@@ -10,7 +11,7 @@ const Filter = () => {
         {/* breadcrumbs */}
 
         {/* Filter head */}
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between pb-5">
           <div className="">
             <h3 className="text-[2.5rem] font-semibold">Tuxedo Suits</h3>
             <div className="font-light text-[12px]">78 items</div>
@@ -28,7 +29,17 @@ const Filter = () => {
         {/* Filter head */}
 
         <div className="grid grid-cols-8 gap-4 py-5">
-          <div className="col-span-2">01</div>
+          <div className="col-span-2">
+            <div className="border-b capitalize flex justify-between items-center pb-3">
+              <p className="font-semibold">Filter</p>
+              <div className="text-[12px] font-light underline cursor-pointer">
+                clear all
+              </div>
+            </div>
+
+            {/* {First Filter} */}
+            <FilterTab />
+          </div>
           <div className="col-span-6 ">
             <div className="grid grid-cols-3 gap-10">
               {new Array(6).fill(0).map((index) => {
